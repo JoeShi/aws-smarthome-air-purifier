7259bc5fcf3f3508e0fc4d7405e33174d4653a0f
qiaoshi <qiaoshi@amazon.com>
2018-05-29 17:38:58 +0800

    optimize code

--- fan.js
+++ fan.js
@@ -3,13 +3,18 @@
 const Gpio = require('onoff').Gpio;
 const fan = new Gpio(17, 'low'); // BCM number
 const buzzer = new Gpio(12, 'high'); // BCM number, hight is off
-let status = 'off'  // current status of fan
+let currentStatus = 'off'  // current status of fan
 
-
+/**
+ * Toggle a buzzer with on|off status.
+ * @param {String} status on|off.
+ * @param {Number} delay delay in ms.
+ * @returns {Promise<>}
+ */
 async function toggleBuzzer(status = 'off', delay = 50){
-  if (status === 'off') {
+  if (status === 'off' && currentStatus === 'on') {
     buzzer.writeSync(1)
-  } else if (status === 'on') {
+  } else if (status === 'on' && currentStatus === 'off') {
     buzzer.writeSync(0)
   }
 
@@ -20,21 +25,21 @@ async function toggleBuzzer(status = 'off', delay = 50){
   }))
 }
 
-toggleBuzzer('on', 50).then(() => {
-  return toggleBuzzer('off', 50)
-}).then(() => {
-  return toggleBuzzer('on', 50)
-}).then(() => {
-  return toggleBuzzer('off', 50)
-}).then(() => {
-  console.log('finished')
-})
-
-module.exports.start = function() {
+module.exports.start = async function() {
   fan.writeSync(0)
+  await toggleBuzzer('on', 50)
+  await toggleBuzzer('off', 50)
+  await toggleBuzzer('on', 50)
+  await toggleBuzzer('off', 50)
+  console.log('fan started...')
+  currentStatus = 'on'
 }
 
-module.exports.stop = function () {
+module.exports.stop = async function () {
   fan.writeSync(1)
+  await toggleBuzzer('on', 150)
+  await toggleBuzzer('off', 50)
+  console.log('fan stopped...')
+  currentStatus = 'off'
 }
 
--- index.js
+++ index.js
@@ -5,65 +5,63 @@ const ThingName = 'air-purifier-1';
 const serial = require('./serial');
 const fan = require('./fan');
 
-fan.start();
+const thingShadow = AWSIoT.thingShadow({
+  keyPath: './certs/private.pem.key',
+  certPath: './certs/certificate.pem.crt',
+  caPath: './certs/ca.pem',
+  clientId: ThingName,
+  host: 'abty4kifln98q.iot.ap-northeast-1.amazonaws.com'
+})
 
-setTimeout(() => {
-  fan.stop();
-}, 3000)
+thingShadow.register(ThingName, () => {
+  const fanState = {
+    state: {
+      reported: {
+        fan: "off"
+      }
+    }
+  }
 
+  const clientTokenUpdate = thingShadow.update(ThingName, fanState)
 
-//
-// const thingShadow = AWSIoT.thingShadow({
-//   keyPath: './certs/private.pem.key',
-//   certPath: './certs/certificate.pem.crt',
-//   caPath: './certs/ca.pem',
-//   clientId: ThingName,
-//   host: 'abty4kifln98q.iot.ap-northeast-1.amazonaws.com'
-// })
-//
-// thingShadow.register(ThingName, () => {
-//   const fanState = {
-//     state: {
-//       reported: {
-//         fan: "off"
-//       }
-//     }
-//   }
-//
-//   const clientTokenUpdate = thingShadow.update(ThingName, fanState)
-//
-//   if (clientTokenUpdate === null) {
-//     console.log('update shadow failed, operation still in progress')
-//   }
-//
-//   console.log('register thing shadow successfully!')
-// })
-//
-// thingShadow.on('delta', (thingName, stateObject) => {
-//   console.log(stateObject)
-//   if (stateObject.state && stateObject.state.fan) {
-//     const status = stateObject.state.fan
-//     toggleFan(status)
-//     const newState = {
-//       state: {
-//         reported: {
-// 	        fan: status
-// 	      }
-//       }
-//     }
-//     console.log(JSON.stringify(newState))
-//     thingShadow.update(thingName, newState)
-//   } else {
-//     console.log('state is same')
-//   }
-// })
-//
-// setInterval(() => {
-//   const density = {
-//     value: serial.dustDensity(),
-//     time: new Date()
-//   }
-//   thingShadow.publish('/airPurifiers/' + ThingName + '/dustDensity', JSON.stringify(density), function(err) {
-//     if (err) { console.error(err) }
-//   })
-// }, 3 * 1000)
+  if (clientTokenUpdate === null) {
+    console.log('update shadow failed, operation still in progress')
+  }
+
+  console.log('register thing shadow successfully!')
+})
+
+thingShadow.on('delta', (thingName, stateObject) => {
+  console.log(stateObject)
+  if (stateObject.state && stateObject.state.fan) {
+    const status = stateObject.state.fan
+    const newState = {
+      state: {
+        reported: {
+          fan: status
+        }
+      }
+    }
+    if (status === 'on') {
+      fan.start().then(() => {
+        thingShadow.update(thingName, newState)
+      })
+    } else if (status === 'off') {
+      fan.stop().then(() => {
+        thingShadow.update(thingName, newState)
+      })
+    }
+  } else {
+    console.log('state is same')
+  }
+})
+
+setInterval(() => {
+  const density = {
+    value: serial.dustDensity(),
+    time: new Date()
+  }
+  thingShadow.publish('/airPurifiers/' + ThingName + '/dustDensity', JSON.stringify(density), function(err) {
+    if (err) { console.error(err) }
+  })
+}, 3 * 1000)
